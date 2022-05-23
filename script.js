c618fec16b8efc16ace340177e7500bd9224f627
Lukas-technoledge <chialukaonuoha@gmail.com>
2022-05-23 03:38:15 -0700

    finished project

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const quotes = [
     {
-        name: 'author 1',
-        quote: 'quote 1' 
+        name: 'Nelson Mandela',
+        quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.' 
     },
     {
-        name: 'author 2',
-        quote: 'quote 2' 
+        name: 'Walt Disney',
+        quote: 'The way to get started is to quit talking and begin doing.' 
     },
     {
-        name: 'author 3',
-        quote: 'quote 3' 
+        name: 'Eleanor Roosevelt',
+        quote: 'If life were predictable it would cease to be life, and be without flavor. ' 
     },
     {
-        name: 'author 4',
-        quote: 'quote 4' 
+        name: 'John Lennon',
+        quote: "Life is what happens when you're busy making other plans." 
     },
     {
-        name: 'author 5',
-        quote: 'quote 5' 
+        name: 'James Cameron',
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success." 
+    },
+    {
+        name: 'Oprah Winfrey',
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough." 
+    },
+    {
+        name: 'Steve Jobs',
+        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking." 
     }
 ]
 
